ed517f6d440642deb969de58ecd506b0bbd5af5e
freya <freyavalencia5@gmail.com>
2024-06-26 22:36:30 +0700

    products product.html

---
+++ home.js
@@ -0,0 +1,27 @@
+const CollectionsProduct = [
+    {
+        title : 'Special SUMMER Collection',
+        picture : 'summer.png',
+        product : 'Watermelon Soap',
+        desc : 'Rejuvenate your sun-kissed skin with Watermelon Soap. Infused with soothing botanicals. rich in vitamin C, A, and B6 which can hydrates and revitalizes after a long day in the sun, leaving you feeling refreshed and ready for the next adventure. Perfect for maintaining your summer glow!',
+    },
+    {
+        title : 'Fall Collection',
+        picture : 'autumn.png',
+        product : 'Golden Hour Soap',
+        desc : 'Refresh your skin with Golden Hour Soap. Infused with cardamom, amber, and balsam fir, its warm, spicy, and slightly sweet aroma captures the essence of fall. This soothing, handcrafted blend deeply nourishes after a crisp day outdoors, leaving your skin radiant and your senses enchanted. Perfect for embracing the cozy spirit of autumn!',
+    },
+    {
+        title : 'Winter Collection',
+        picture : 'winter.png',
+        product : 'Snowflakes Soap',
+        desc : 'Nourish and protect your skin from the harsh winter chill with Snowflakes Soap. Formulated with a rich blend of lavender, musk, pine, fir, and vanilla, this soap deeply moisturizes dry, cold-weather skin. The warm, comforting scents of pine, fir, cinnamon, and vanilla envelop your senses, leaving your skin soft, nourished, and glowing all season long!'
+    }
+
+]
+
+
+let Collection = document.getElementById('CollectionsContainer')
+let summerBg = document.getElementById('summerBg')
+
+let summerTitle = document.createElement
--- script.js
+++ script.js
@@ -26,44 +26,50 @@ function loadComponent(id,comp){
 
 const soapProduct = [
     {
-        picture : 'agarwood.png',
-        title : 'Agarwood Soap',
-        price : 42.000,
+        picture : 'winter.png',
+        title : 'Winter Soap',
+        price : `Rp${58000}`,
+    },
+    {
+        picture : 'summer.png',
+        title : 'Summer Soap',
+        price : `Rp${58000}`,
     },
     {
         picture : 'autumn.png',
         title : 'Autumn Soap',
-        price : 58.000,
+        price : `Rp${58000}`,
     },
+    {
+        picture : 'agarwood.png',
+        title : 'Agarwood Soap',
+        price : `Rp${42000}`,
+    },
+    
     {
         picture : 'cedarwood.png',
         title : 'Cedarwood Soap',
-        price : 42.000,
+        price : `Rp${42000}`,
     },
     {
         picture : 'chamomile.png',
         title : 'Chamomile Soap',
-        price : 42.000,
+        price : `Rp${42000}`,
     },
     {
         picture : 'eucalyptus.png',
         title : 'Eucalyptus Soap',
-        price : 42.000,
+        price : `Rp${42000}`,
     },
     {
         picture : 'lemongrass.png',
         title : 'Lemongrass Soap',
-        price : 42.000,
+        price : `Rp${42000}`,
     },
     {
         picture : 'sandalwood.png',
         title : 'Sandalwood Soap',
-        price : 42.000,
-    },
-    {
-        picture : 'winter.png',
-        title : 'Winter Soap',
-        price : 58.000,
+        price : `Rp${42000}`,
     },
 ]
 
@@ -74,28 +80,32 @@ for(i=0;i<soapProduct.length;i++){
     
     let card = document.createElement("div")
     card.style.width = "300px"
-    card.style.height =
-    card.className ='border border-bottom-2 border-start-2 border-end-2 m-5 mb-4 p-0'
+    card.style.height = "430px"
+    card.className ='card border border-bottom-2 border-start-2 border-end-2 m-5 mb-4 p-0'
 
     let cardImage = document.createElement('img')
     cardImage.src = `/assets/pictures/productPicture/${soapProduct[i].picture}`
+    cardImage.style.height = "300px"
     cardImage.className = 'col card-img-top'
 
     let cardBody = document.createElement('div')
-    cardBody.className = 'd-flex justify-content-center'
+    cardBody.className = 'p-2'
 
     let cardTitle = document.createElement("h4")
     cardTitle.className ="col card-title fw-bold fs-4 "
     cardTitle.innerHTML = soapProduct[i].title 
 
     let cardPrice = document.createElement("h5")
-    cardPrice.className ="card-text display-5"
+    cardPrice.className ="card-text text-dark-emphasis fs-6"
+    cardPrice.innerHTML = soapProduct[i].price
 
     let cardButton = document.createElement('button')
-    cardButton.className ='btn'
+    cardButton.className ='btn btn-sm bg-success-subtle rounded-0'
+    cardButton.style.width = "100%"
+
     let cardA = document.createElement('a')
     cardA.innerHTML = 'See details'
-    cardA.className ='fw-semibold text-success '
+    cardA.className ='fw-semibold text-dark link-underline link-underline-opacity-0'
 
     productList.appendChild(card)
     card.appendChild(cardImage)
